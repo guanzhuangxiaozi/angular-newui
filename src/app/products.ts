fdb453ca1330855a81e4683ca1a6e12f368d3f02
LinYoung <1054623914@qq.com>
2020-06-04 15:40:01 +0800

    4 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,18 +1,18 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: 'Item 0001',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A great phone with one of the best screens'
   },
   {
-    name: 'Phone Mini',
+    name: 'Item 0002',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
-    name: 'Phone Standard',
+    name: 'Item 0003',
     price: 299,
-    description: ''
+    description: 'A great phone with one of the best system'
   }
 ];
 
