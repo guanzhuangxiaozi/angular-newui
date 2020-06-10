edc4baffa0c819b2cb824140e2f6533355368ce8
guanzhuangxiaozi <1054623914@qq.com>
2020-06-10 13:53:46 +0800

    1 file modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
-import { OverlayComponent } from './overlay/overlay.component';
 
 @NgModule({
   imports: [
@@ -23,8 +22,7 @@ import { OverlayComponent } from './overlay/overlay.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent},
       { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
-      { path: 'test', component:OverlayComponent}
+      { path: 'shipping', component: ShippingComponent }
     ])
   ],
   declarations: [
